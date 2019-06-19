7b2b7d32fd8206ad2830dfaa3b43df5c3b3d2496
ioqren <corriestoddard@gmail.com>
2019-06-18 21:31:37 -0700

    Update css

--- day1/src/App.js
+++ day1/src/App.js
@@ -57,20 +57,14 @@ class App extends React.Component {
     return (
 
       <div>
-        <div class="row">
           <div id="display">{this.state.currSound}</div>
-        </div>
-        <div class="row">
+        <div id="drum-pads">
           <div id='1' class="drum-pad" onClick={() => this.handleInput('Q')}><audio className="clip" id="Q" src='https://s1.vocaroo.com/media/download_temp/Vocaroo_s1hDcXn5aM5J.mp3' />Q</div>
           <div id='2' class="drum-pad" onClick={() => this.handleInput('W')}><audio className="clip" id="W" src='https://s1.vocaroo.com/media/download_temp/Vocaroo_s1nPHw6YCZsc.mp3' />W</div>
           <div id='3' class="drum-pad" onClick={() => this.handleInput('E')}><audio className="clip" id="E" src='https://s1.vocaroo.com/media/download_temp/Vocaroo_s13XNsiLEhpm.mp3' />E</div>
-        </div>
-        <div class="row">
           <div id='4' class="drum-pad" onClick={() => this.handleInput('A')}><audio className="clip" id="A" src='https://s1.vocaroo.com/media/download_temp/Vocaroo_s1pG5s2rs7vk.mp3' />A</div>
           <div id='5' class="drum-pad" onClick={() => this.handleInput('S')}><audio className="clip" id="S" src='https://s0.vocaroo.com/media/download_temp/Vocaroo_s0gdOxlohw8x.mp3' />S</div>
           <div id='6' class="drum-pad" onClick={() => this.handleInput('D')}><audio className="clip" id="D" src='https://s0.vocaroo.com/media/download_temp/Vocaroo_s0U5gyfQ2k75.mp3' />D</div>
-        </div>
-        <div class="row">
           <div id='7' class="drum-pad" onClick={() => this.handleInput('Z')}><audio className="clip" id="Z" src='https://s1.vocaroo.com/media/download_temp/Vocaroo_s17auWehXfUm.mp3' />Z</div>
           <div id='8' class="drum-pad" onClick={() => this.handleInput('X')}><audio className="clip" id="X" src='https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' />X</div>
           <div id='9' class="drum-pad" onClick={() => this.handleInput('C')}><audio className="clip" id="C" src='https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' />C</div></div>
